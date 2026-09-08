729194a3e3927556499bed372939dc44a9dfa3ab
Jonatas-01 <jonatasmc02@gmail.com>
2026-09-08 12:40:30 +0100

    Add global style and fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+        'Figtree-Regular': require('@/assets/fonts/Figtree-Regular.ttf'),
+        'Figtree-Medium': require('@/assets/fonts/Figtree-Medium.ttf'),
+        'Figtree-SemiBold': require('@/assets/fonts/Figtree-SemiBold.ttf'),
+        'Figtree-Bold': require('@/assets/fonts/Figtree-Bold.ttf'),
+        'Figtree-ExtraBold': require('@/assets/fonts/Figtree-ExtraBold.ttf'),
+        'Figtree-Black': require('@/assets/fonts/Figtree-Black.ttf'),
+        'SpaceMono-Regular': require('@/assets/fonts/SpaceMono-Regular.ttf'),
+        'SpaceMono-Bold': require('@/assets/fonts/SpaceMono-Bold.ttf')
+    });
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return <Stack />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-xl font-bold text-blue-500">
+            <Text className="font-sans text-title font-ui-700">
                 Welcome to Nativewind!
             </Text>
         </View>
